96b7ad4361352224059625aca5acac26c9648fb1
Eduardo Riesco <eduardo.riesco@gmail.com>
2026-03-17 14:54:31 +0100

    fix: file name convention

--- 2023/python/01/test_Trebuchet.py
+++ 2023/python/01/test_Trebuchet.py
@@ -1,44 +1,45 @@
 import unittest
-import Trebuchet as Tr
+
+import trebuchet as tr
 
 
 class TestTrebuchet(unittest.TestCase):
 
     # Part one
     def test_01(self):
-        self.assertEqual(12, Tr.calibrate('1abc2'))
+        self.assertEqual(12, tr.calibrate('1abc2'))
 
     def test_02(self):
-        self.assertEqual(38, Tr.calibrate('pqr3stu8vwx'))
+        self.assertEqual(38, tr.calibrate('pqr3stu8vwx'))
 
     def test_03(self):
-        self.assertEqual(15, Tr.calibrate('a1b2c3d4e5f'))
+        self.assertEqual(15, tr.calibrate('a1b2c3d4e5f'))
 
     def test_04(self):
-        self.assertEqual(77, Tr.calibrate('treb7uchet'))
+        self.assertEqual(77, tr.calibrate('treb7uchet'))
 
     # Part two
     def test_05(self):
-        self.assertEqual(29, Tr.calibrate('two1nine'))
+        self.assertEqual(29, tr.calibrate('two1nine'))
 
     def test_06(self):
-        self.assertEqual(83, Tr.calibrate('eightwothree'))
+        self.assertEqual(83, tr.calibrate('eightwothree'))
 
     def test_07(self):
-        self.assertEqual(13, Tr.calibrate('abcone2threexyz'))
+        self.assertEqual(13, tr.calibrate('abcone2threexyz'))
 
     def test_08(self):
-        self.assertEqual(24, Tr.calibrate('xtwone3four'))
+        self.assertEqual(24, tr.calibrate('xtwone3four'))
 
     def test_09(self):
-        self.assertEqual(42, Tr.calibrate('4nineeightseven2'))
+        self.assertEqual(42, tr.calibrate('4nineeightseven2'))
 
     def test_10(self):
-        self.assertEqual(14, Tr.calibrate('zoneight234'))
+        self.assertEqual(14, tr.calibrate('zoneight234'))
 
     def test_11(self):
-        self.assertEqual(76, Tr.calibrate('7pqrstsixteen'))
+        self.assertEqual(76, tr.calibrate('7pqrstsixteen'))
 
     # Additional tests
     def test_12(self):
-        self.assertEqual(51, Tr.calibrate('jjhxddmg5mqxqbgfivextlcpnvtwothreetwonerzk'))
+        self.assertEqual(51, tr.calibrate('jjhxddmg5mqxqbgfivextlcpnvtwothreetwonerzk'))
--- 2023/python/03/test_gear_ratios.py
+++ 2023/python/03/test_gear_ratios.py
@@ -1,73 +1,63 @@
 import unittest
-import Gear_ratios as Gr
+
+import gear_ratios as gr
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_get_line_numbers1(self):
-        self.assertEqual(['123'], Gr.get_line_numbers('..123..'))
+        self.assertEqual(['123'], gr.get_line_numbers('..123..'))
 
     def test_get_line_numbers2(self):
-        self.assertEqual(['123', '456'], Gr.get_line_numbers('..123..456..'))
+        self.assertEqual(['123', '456'], gr.get_line_numbers('..123..456..'))
 
     def test_get_line_numbers3(self):
-        self.assertEqual(['123', '456'], Gr.get_line_numbers('..#123..456#..'))
+        self.assertEqual(['123', '456'], gr.get_line_numbers('..#123..456#..'))
 
     def test_get_part_numbers1(self):
-        self.assertEqual([], Gr.get_part_numbers('..123..'))
+        self.assertEqual([], gr.get_part_numbers('..123..'))
 
     def test_get_part_numbers2(self):
-        self.assertEqual(['123'], Gr.get_part_numbers('..#123..'))
+        self.assertEqual(['123'], gr.get_part_numbers('..#123..'))
 
     def test_get_part_numbers3(self):
-        self.assertEqual(['456'], Gr.get_part_numbers('..456#..'))
+        self.assertEqual(['456'], gr.get_part_numbers('..456#..'))
 
     def test_get_part_numbers4(self):
-        self.assertEqual(['123', '456'], Gr.get_part_numbers('..#123..456#..'))
+        self.assertEqual(['123', '456'], gr.get_part_numbers('..#123..456#..'))
 
     def test_get_part_numbers5(self):
-        self.assertEqual(['123', '456'], Gr.get_part_numbers('#123..456#'))
+        self.assertEqual(['123', '456'], gr.get_part_numbers('#123..456#'))
 
     def test_get_part_numbers6(self):
-        self.assertEqual(['123', '456'], Gr.get_part_numbers('#123#456#'))
+        self.assertEqual(['123', '456'], gr.get_part_numbers('#123#456#'))
 
     def test_is_adjacent_to_a_symbol_from_the_same_line1(self):
-        self.assertFalse(Gr.is_adjacent_to_a_symbol_from_the_same_line('..123..', '123'))
+        self.assertFalse(gr.is_adjacent_to_a_symbol_from_the_same_line('..123..', '123'))
 
     def test_is_adjacent_to_a_symbol_from_the_same_line2(self):
-        self.assertTrue(Gr.is_adjacent_to_a_symbol_from_the_same_line('..#123..', '123'))
+        self.assertTrue(gr.is_adjacent_to_a_symbol_from_the_same_line('..#123..', '123'))
 
     def test_is_adjacent_to_a_symbol_from_the_same_line3(self):
-        self.assertTrue(Gr.is_adjacent_to_a_symbol_from_the_same_line('..123#..', '123'))
+        self.assertTrue(gr.is_adjacent_to_a_symbol_from_the_same_line('..123#..', '123'))
 
     def test_is_adjacent_to_a_symbol_from_other_line1(self):
-        self.assertFalse(Gr.is_adjacent_to_a_symbol_from_other_line('..123..', '.......', '123'))
+        self.assertFalse(gr.is_adjacent_to_a_symbol_from_other_line('..123..', '.......', '123'))
 
     def test_is_adjacent_to_a_symbol_from_other_line2(self):
-        self.assertTrue(Gr.is_adjacent_to_a_symbol_from_other_line('.123', '#...', '123'))
+        self.assertTrue(gr.is_adjacent_to_a_symbol_from_other_line('.123', '#...', '123'))
 
     def test_is_adjacent_to_a_symbol_from_other_line3(self):
-        self.assertTrue(Gr.is_adjacent_to_a_symbol_from_other_line('123', '#..', '123'))
+        self.assertTrue(gr.is_adjacent_to_a_symbol_from_other_line('123', '#..', '123'))
 
     def test_is_adjacent_to_a_symbol_from_other_line4(self):
-        self.assertTrue(Gr.is_adjacent_to_a_symbol_from_other_line('123.', '...#', '123'))
-
+        self.assertTrue(gr.is_adjacent_to_a_symbol_from_other_line('123.', '...#', '123'))
 
     # def test_line_adjacency1(self):
     #     self.assertFalse(Gr.line_adjacency('..123..'))
     #
     # def test_line_adjacency2(self):
     #     self.assertTrue(Gr.line_adjacency('..123#.'))
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
